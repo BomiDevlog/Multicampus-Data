a974ebb42c9e05571353baec1aab8242f2a4e812
Bomi <aloe06@gmail.com>
2022-09-14 10:40:52 +0900

    qrcode, ocr영수증 자료

---
+++ practice.py
@@ -0,0 +1,17 @@
+w=40
+h=20
+print('삼각형의 면적 :',  w*h/2)
+
+print('사각형의 면적 :', w*h)
+
+print(1+2+3+4+5+6+7+8+9+10)
+
+print(32/8)
+
+a= int(input('정사각형의 밑변을 입력하시오 : '))
+print('정사각형의 면적 : ', a*a)
+
+n=2
+print('a','n', 'a**n')
+for a in range(2,7):
+    print(a,n,a**n)
